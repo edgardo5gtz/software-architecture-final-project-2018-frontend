4e65fb04e760b699f088e1c7d6a50287e52c4399
Edgardo Gutierrez <edgardo5gtz@gmail.com>
2018-04-21 14:11:41 -0500

    Move App Sidebar logic to new Navbar file.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,9 @@
 import React, { Component } from 'react'
-import {Route , Link } from 'react-router-dom'
-import { Sidebar, Menu, Icon, Image } from 'semantic-ui-react'
-import ImageLogo from './assets/img/achivement-logo.jpg'
+import Navbar from './container/Navbar/Navbar'
 class App extends Component {
     render(){
         return(
-            <Sidebar as={Menu} visible={true} vertical inverted>
-                <Image src={ImageLogo} size='small' circular />
-                <Link to="/habits">
-                    <Menu.Item name='habits'>
-                        <Icon name='home' />
-                        Habits
-                    </Menu.Item>
-                </Link>
-                <Link to="/tasks">
-                    <Menu.Item name='tasks'>
-                        <Icon name='gamepad' />
-                        Tasks
-                    </Menu.Item>
-                </Link>
-                <Link to="/report">
-                    <Menu.Item name='report'>
-                        <Icon name='camera' />
-                        Report
-                    </Menu.Item>
-                </Link>
-                <Link to="/account">
-                    <Menu.Item name='acoount'>
-                        <Icon name='camera' />
-                        Account
-                    </Menu.Item>
-                </Link>
-            </Sidebar>
+            <Navbar/>
         );
     }
 }
