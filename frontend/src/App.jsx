f2a7c82007b3a0d4ab9a1f4a32a1b327fd4fcbf0
Edgardo Gutierrez <edgardo5gtz@gmail.com>
2018-04-21 14:05:14 -0500

    Test links from sidebar items

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,38 @@
 import React, { Component } from 'react'
+import {Route , Link } from 'react-router-dom'
 import { Sidebar, Menu, Icon, Image } from 'semantic-ui-react'
 import ImageLogo from './assets/img/achivement-logo.jpg'
 class App extends Component {
     render(){
-    return(
-        <Sidebar as={Menu} visible={true} vertical inverted>
-            <Image src={ImageLogo} size='small' circular />
-            <Menu.Item name='habits'>
-                <Icon name='home' />
-                Habits
-            </Menu.Item>
-            <Menu.Item name='tasks'>
-                <Icon name='gamepad' />
-                Tasks
-            </Menu.Item>
-            <Menu.Item name='report'>
-                <Icon name='camera' />
-                Report
-            </Menu.Item>
-            <Menu.Item name='acoount'>
-                <Icon name='camera' />
-                Account
-            </Menu.Item>
-        </Sidebar>
-    );
+        return(
+            <Sidebar as={Menu} visible={true} vertical inverted>
+                <Image src={ImageLogo} size='small' circular />
+                <Link to="/habits">
+                    <Menu.Item name='habits'>
+                        <Icon name='home' />
+                        Habits
+                    </Menu.Item>
+                </Link>
+                <Link to="/tasks">
+                    <Menu.Item name='tasks'>
+                        <Icon name='gamepad' />
+                        Tasks
+                    </Menu.Item>
+                </Link>
+                <Link to="/report">
+                    <Menu.Item name='report'>
+                        <Icon name='camera' />
+                        Report
+                    </Menu.Item>
+                </Link>
+                <Link to="/account">
+                    <Menu.Item name='acoount'>
+                        <Icon name='camera' />
+                        Account
+                    </Menu.Item>
+                </Link>
+            </Sidebar>
+        );
     }
 }
 
