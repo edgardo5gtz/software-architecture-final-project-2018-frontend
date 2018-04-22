a306131be1779e302875fcc8e597f7e03f6f0d19
Edgardo Gutierrez <edgardo5gtz@gmail.com>
2018-04-22 12:21:17 -0500

    Add items and style to the Home page

--- frontend/src/container/Home/Home.jsx
+++ frontend/src/container/Home/Home.jsx
@@ -1,14 +1,29 @@
 // Import react global packages
 // Import react global packages
 import React, { Component } from 'react'
-import { Grid } from 'semantic-ui-react'
+import { Grid, Header, Button } from 'semantic-ui-react'
 import { Route, Switch } from 'react-router-dom'
 
 class Home extends Component {
     render() {
         return (
-            <Grid textAlign="center">
-                Home
+            <Grid>
+                <Grid.Row>
+                    <Grid.Column floated="right" width={5}>
+                        <Button content='Log In' primary />
+                        <Button content='Sign Up' secondary />
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row verticalAlign="middle" centered>
+                    <Grid.Column>
+                        <Header as="h1" textAlign="center">
+                            Home page
+                            <Header.Subheader>
+                                Catchy phrase all your dreams come true blablabla
+                            </Header.Subheader>
+                        </Header>
+                    </Grid.Column>
+                </Grid.Row>
             </Grid>
         );
     }
